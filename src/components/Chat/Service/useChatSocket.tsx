23fe0ff46aac2572687126116ed478a6fa5c1bab
Rıdvan Altunel <ridvan.altunel@gmail.com>
2024-07-02 03:43:41 +0300

    extra logs

--- src/components/Chat/Service/useChatSocket.tsx
+++ src/components/Chat/Service/useChatSocket.tsx
@@ -146,6 +146,8 @@ const useChatSocket = (
                     new Date().getSeconds(),
             );
             console.log('isUserIdle', isUserIdle);
+            console.log('isChatOpen', isChatOpen);
+            console.log('check', isChatOpen && !isUserIdle);
             if (isChatOpen && !isUserIdle) {
                 console.log('gonna heartbeat');
                 setHeartbeat(new Date().getTime());
