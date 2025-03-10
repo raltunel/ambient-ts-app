12b065171fadfbddbc0c127e8762f260453550d0
tcoskun <tcoskun@proven.com.tr>
2025-03-10 14:39:54 +0300

    changed liquidity default colors according to correct label names

--- src/pages/Chart/ChartSettings/ChartSettingsContent.tsx
+++ src/pages/Chart/ChartSettings/ChartSettingsContent.tsx
@@ -411,8 +411,8 @@ export default function ChartSettingsContent(props: ContextMenuContentIF) {
             selection: 'Liquidity Area',
             actionHandler: 'liq',
             action: handleCandleColorPicker,
-            downColor: 'liqBidColor',
-            upColor: 'liqAskColor',
+            downColor: 'liqAskColor',
+            upColor: 'liqBidColor',
             exclude: ['futa'],
         },
         {
